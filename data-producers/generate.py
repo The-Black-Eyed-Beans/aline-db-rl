8ad14f0c08a3199937a50222b0e3f1ff2808eae0
Ricky Lopez <rllopez718@gmail.com>
2022-01-15 18:59:03 -0500

    Continued refactoring.

--- data-producers/generate.py
+++ data-producers/generate.py
@@ -17,6 +17,11 @@ transaction_type = ["DEPOSIT","WITHDRAWAL","PURCHASE","PAYMENT","REFUND","VOID",
 transaction_method = ["ACH","ATM","CREDIT_CARD","DEBIT_CARD","APP"]
 
 
+'''[DEPRECATED] function to manually remove all symbols not accepted by the regex expression used by Aline. [DEPRECATED]'''
+def password_cleaner(password):
+    new_password = password.replace("+","").replace("(","").replace(")","").replace("+","")
+    return new_password
+
 '''generates a random phone number in the proper format.'''
 def phone_number():
 
@@ -71,7 +76,7 @@ def applicant():
 
     return applicant_json
 
-'''generates application information and returns it in the proper json format for microservice.'''
+'''generates application information and returns it in the proper json format for microservice. Requires applicant id'''
 def application(applicant_id):
 
     application_json = {
@@ -82,6 +87,72 @@ def application(applicant_id):
 
     return application_json
 
-'''generates user information adn returns it in the proper json format for microservice.'''
+'''generates user information and returns it in the proper json format for microservice.'''
+'''
+    applicant_info keys:
+        first_name
+        last_name
+        email
+        phone
+        membership_id
+        ssn
+'''
 def user(**applicant_info) :
     
+    user_json = {
+        "role":"member",
+        "username": fake.profile(fields=['username'])['username'] + str(randint(10,99)),
+        "password": password(),
+        "firstName": applicant_info["first_name"],
+        "lastName": applicant_info["last_name"],
+        "email": applicant_info['email'],
+        "phone": applicant_info["phone"],
+        "membershipId": applicant_info['membership_id'],
+        "lastFourOfSSN": str(applicant_info["ssn"])[7:11]
+    }
+
+    return user_json
+
+'''generates transaction information and returns it in the proper json format for microservice. Requires account number'''
+def transaction(account_number):
+
+    transaction_json ={
+        "type": transaction_type[randint(0,7)],
+        "method": transaction_method[randint(0,4)],
+        "amount": randint(1000, 5000),
+        "merchantCode": 'hj1234',
+        "merchantName": 'Hillcrest ATM',
+        "description": 'High class merchant',
+        "cardNumber": None,
+        "accountNumber": account_number
+    }
+
+    return transaction_json
+
+'''generates bank information and returns it in the proper json format for microservice.'''
+def bank():
+
+    bank_json  = {
+        "routingNumber": "".join([str(randint(0,9)) for _ in range(9)]),
+        "address": fake.street_address(),
+        "city": fake.city(),
+        "state": fake.state(),
+        "zipcode": fake.zipcode()
+    }
+
+    return bank_json
+
+'''generates branch information and returns it in the proper json format for microservice. requires bank json'''
+def branch(bank_json):
+
+    branch_json = {
+        "name": fake.word() + "branch",
+        "phone": phone_number,
+        "address": bank_json["address"],
+        "city": bank_json['city'],
+        "state": bank_json['state'],
+        "zipcode": bank_json['zipcode'],
+        "bankID": bank.json()['id']
+    }
+
+    return branch_json
--- data-producers/request.py
+++ data-producers/request.py
@@ -9,7 +9,10 @@ from datetime import date
 import time
 from dotenv import load_dotenv
 
-''' Uses login credentials to send login request to user microservice. '''
+'''
+    Uses login credentials to send login request to user microservice. If successful, returns bearer token in the form of a formatted dictionary defined
+    by the 'requests' python module. 
+'''
 def login(username, password, user_host):
     logging.info(f"attempting login to application using admin credentials.. ")
 
@@ -47,6 +50,31 @@ def login(username, password, user_host):
     
     return login
 
+'''Sends POST request to respective microservice with entity json to populate database.'''
+def post_entity(json, headers, host, route):
+    logging.debug(f"attempting to send POST request to {host+route}...")
+
+    if not host:
+        logging.warning("host was not found. Please check environment.")
+        return 0
+    
+    logging.debug("Sending request to server...")
+
+    response = requests.post(host+route, json=json, headers=headers)
+
+    if response.status_code in range(200, 300):
+        logging.debug(f"STATUS CODE: {response.status_code} ({response.reason})")
+    elif response.status_code in range(300, 400):
+        logging.error(f"CLIENT ERROR CODE {response.status_code} ({response.reason})")
+        return 0
+    elif response.status_code in range(400, 500):
+        logging.error(f"SERVER ERROR CODE {response.status_code} ({response.reason})")
+        return 0
+    else:
+        logging.info(f"STATUS CODE: {response.status_code} ({response.reason})")
+
+    return response
+
 ''' Sends request to underwriter microservice with applicant json to create a new applicant.'''
 def add_applicant(applicant_json, bearer, underwriter_host):
     logging.debug(f"attempting to send applicant request..")
@@ -70,5 +98,59 @@ def add_applicant(applicant_json, bearer, underwriter_host):
     else:
         logging.info(f"STATUS CODE: {response.status_code} ({response.reason})")
 
-    
+    return 1
 
+    
+''' Sends request to user microservice with user json to create a new user.'''
+def add_user(user_json, bearer, user_host):
+    logging.debug("attempting to send user request...")
+
+    if user_host is None:
+        logging.warning(f"user host was not found. Please check environment.")
+        return 0
+    
+    logging.debug("Sending request to server...")
+
+    response = requests.post(user_host, json=user_json, headers=bearer)
+
+    if response.status_code in range(200, 300):
+        logging.debug(f"STATUS CODE: {response.status_code} ({response.reason})")
+    elif response.status_code in range(300, 400):
+        logging.error(f"CLIENT ERROR CODE {response.status_code} ({response.reason})")
+        return 0
+    elif response.status_code in range(400, 500):
+        logging.error(f"SERVER ERROR CODE {response.status_code} ({response.reason})")
+        return 0
+    else:
+        logging.info(f"STATUS CODE: {response.status_code} ({response.reason})")
+
+    return 1
+
+''' Sends request to transaction microservice with transaction json to create a new transaction.'''
+def add_transaction(transaction_json, bearer, transaction_host):
+    logging.debug("attempting to send transaction request...")
+
+    if transaction_host is None:
+        logging.warning(f"transaction host was not found. Plesae check environment.")
+        return 0
+    
+    logging.debug("Sending request to server...")
+
+    response = requests.post(transaction_host, json=transaction_json, headers=bearer)
+
+    if response.status_code in range(200, 300):
+        logging.debug(f"STATUS CODE: {response.status_code} ({response.reason})")
+    elif response.status_code in range(300, 400):
+        logging.error(f"CLIENT ERROR CODE {response.status_code} ({response.reason})")
+        return 0
+    elif response.status_code in range(400, 500):
+        logging.error(f"SERVER ERROR CODE {response.status_code} ({response.reason})")
+        return 0
+    else:
+        logging.info(f"STATUS CODE: {response.status_code} ({response.reason})")
+
+    return 1
+
+'''Sends request to bank microservice with bank json to create a new bank.'''
+def add_bank():
+    pass
